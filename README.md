6defe78d9bca10efccd4e56054c5249b9417e1b9
Tpj-root <trichy_hackerspace@outlook.com>
2024-11-14 14:58:17 -0500

    start

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # QTAnimations
-Desktop funimation
+
+**Desktop funimation**
+
+
+Telegram --> AnimatedSticker.tgs  to gif
+
+https://github.com/kamaravichow/tgs-viewer.git
+
+
+
+
+
+
+
+
+**try_it_extra**
+
+tux.gif
+
+spider.gif
+
+
+
+**how to run**
+
+```
+python3 sunflower.py
+```
+
+![Sunflower Animation](gif/sunflower.gif)
+
+```
+python3 moving_man.py
+```
+
+![moving_man](gif/moving_man.gif)
+
+
+![joker](gif/joker.gif )
+
+
+Desktop_ani.mp4  joker.gif       solar_system.gif  sunflower.gif
+family.gif       moving_man.gif  spider.gif        tux.gif
+
+
+
+
+
+
+
+
+
+
+
+**how it works**
+
+[Watch the video](https://github.com/yourusername/yourrepo/blob/main/video.mp4)
+
+
+
+
+
+
+
+
---
+++ moving_man.py
@@ -0,0 +1,42 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtGui import QMovie
+from PyQt5.QtCore import Qt
+
+app = QApplication(sys.argv)
+
+# Create the main window
+window = QMainWindow()
+
+# Make the window transparent and always on top
+window.setAttribute(Qt.WA_TranslucentBackground, True)
+window.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+
+# Create a central widget and set a layout
+central_widget = QWidget()
+layout = QVBoxLayout(central_widget)
+
+# Add an animated GIF
+label = QLabel()
+movie = QMovie('gif/moving_man.gif')  # Relative path from the script
+label.setMovie(movie)
+movie.start()
+layout.addWidget(label)
+
+window.setCentralWidget(central_widget)
+
+# Set a fixed size for the window if needed
+window.setFixedSize(movie.currentPixmap().size())  # Resize the window to fit the GIF
+
+# Move the window to the right side corner
+screen_geometry = app.primaryScreen().geometry()
+window_geometry = window.geometry()
+right_x = screen_geometry.width() - window_geometry.width()  # X position for right side
+bottom_y = screen_geometry.height() - window_geometry.height()  # Y position for bottom
+window.move(right_x, bottom_y)
+
+window.show()
+
+# Start the event loop
+sys.exit(app.exec())
+
